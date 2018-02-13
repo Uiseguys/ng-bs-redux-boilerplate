42ad1866f2a46b39c1a2cfd36d41df4e584c7a5a
Divyesh Bhalala <bhalaladivs@gmail.com>
2018-02-13 20:11:14 +0530

    integrating stencil component into angular

--- src/app/shared/environment/environment.module.ts
+++ src/app/shared/environment/environment.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatusComponent } from './components/status/status.component';
 import { EnvironmentObservers } from './api/observer';
 import { EnvironmentActions } from './api/actions';
 
 @NgModule({
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   imports: [ CommonModule ],
   declarations: [ StatusComponent ],
   providers: [ EnvironmentObservers, EnvironmentActions ],
