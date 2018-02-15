033e7154d3e85b81f2c95d37ada3091db21d9de8
ahadortiz <ahadortiz@outlook.com>
2018-02-15 21:05:06 +0800

    language change function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,53 @@
-import { NgModule, ApplicationRef, APP_INITIALIZER, LOCALE_ID, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {
+  NgModule,
+  ApplicationRef,
+  APP_INITIALIZER,
+  LOCALE_ID,
+  CUSTOM_ELEMENTS_SCHEMA
+} from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { I18NextModule, I18NEXT_SERVICE, ITranslationService } from 'angular-i18next';
-import * as i18nextXHRBackend from 'i18next-xhr-backend';
-import * as i18nextLanguageDetector from 'i18next-browser-languagedetector';
-
+import {
+  I18NextModule,
+  I18NEXT_SERVICE,
+  ITranslationService
+} from "angular-i18next";
+import * as i18nextXHRBackend from "i18next-xhr-backend";
+import * as i18nextLanguageDetector from "i18next-browser-languagedetector";
 
 /*
  * Platform and Environment providers/directives/pipes
  */
 // import { ENV_PROVIDERS } from './environment';
 
-import { AppComponent } from './app.component';
-import { ViewsModule } from './views/views.module';
-import { NavigationModule } from './shared/navigation/navigation.module';
-import { StatusModule } from './shared/environment/environment.module';
-import { I18nObservers } from './shared/i18n/observer';
-import { I18nActions } from './shared/i18n/actions';
-import { DashboardObservers } from './views/dashboard/api/observer';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TokenInterceptor } from './shared/authorization/token.interceptor';
-import { AuthService } from './shared/authorization/authentication.service';
-import { ToDoObservers } from './features/todos/api/observer';
-import { LocalSettingsObservers } from './views/local-settings/api/observer';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from "./app.component";
+import { ViewsModule } from "./views/views.module";
+import { NavigationModule } from "./shared/navigation/navigation.module";
+import { StatusModule } from "./shared/environment/environment.module";
+import { I18nObservers } from "./shared/i18n/observer";
+import { I18nActions } from "./shared/i18n/actions";
+import { DashboardObservers } from "./views/dashboard/api/observer";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { TokenInterceptor } from "./shared/authorization/token.interceptor";
+import { AuthService } from "./shared/authorization/authentication.service";
+import { ToDoObservers } from "./features/todos/api/observer";
+import { LocalSettingsObservers } from "./views/local-settings/api/observer";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { AsideNavComponent } from './views/dashboard/components/aside-nav/aside-nav.component';
+import { AsideNavComponent } from "./views/dashboard/components/aside-nav/aside-nav.component";
 
 const i18nextOptions = {
-  whitelist: ['de'],
-  fallbackLng: 'de',
+  whitelist: ["de", "en"],
+  fallbackLng: localStorage.getItem("lang") || "en",
   debug: true, // set debug?
   returnEmptyString: false,
-  ns: [
-    'default',
-  ],
-  defaultNS: 'default',
+  ns: ["default"],
+  defaultNS: "default",
   backend: {
-    loadPath: function (langs, ns) {
+    loadPath: function(langs, ns) {
       // return 'assets/locales/{{lng}}.{{ns}}.json';
-      return 'http://18.196.175.166:3000/api/Published/361c9e81-8d78-4fff-b53e-21cdc47846c8/{{lng}}/{{ns}}';
+      return "http://18.196.175.166:3000/api/Published/361c9e81-8d78-4fff-b53e-21cdc47846c8/{{lng}}/{{ns}}";
     }
   },
   interpolation: {
@@ -49,23 +56,23 @@ const i18nextOptions = {
   // lang detection plugin options
   detection: {
     // order and from where user language should be detected
-    order: ['cookie'],
+    order: ["cookie"],
 
     // keys or params to lookup language from
-    lookupCookie: 'lang',
+    lookupCookie: "lang",
 
     // cache user language on
-    caches: ['cookie'],
+    caches: ["cookie"],
 
     // optional expire and domain for set cookie
     cookieMinutes: 10080, // 7 days
-    cookieDomain: 'location.href.host'
+    cookieDomain: "location.href.host"
   }
 };
 
 export function appInit(i18next: ITranslationService) {
   return () => {
-    const promise: Promise<any> = i18next  // was    let promise: Promise<I18NextLoadResult> = i18next
+    const promise: Promise<any> = i18next // was    let promise: Promise<I18NextLoadResult> = i18next
       .use(i18nextXHRBackend)
       .use(i18nextLanguageDetector)
       .init(i18nextOptions);
@@ -88,23 +95,22 @@ export const I18N_PROVIDERS = [
     provide: LOCALE_ID,
     deps: [I18NEXT_SERVICE],
     useFactory: localeIdFactory
-  },
+  }
 ];
 
 /**
  * `AppModule` is the main entry point into Angular2's bootstraping process
  */
 @NgModule({
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
   declarations: [
     // app
     AppComponent,
     AsideNavComponent
   ],
-  imports: [ // import Angular's modules
+  imports: [
+    // import Angular's modules
     // core
     BrowserModule,
     FormsModule,
@@ -116,9 +122,9 @@ export const I18N_PROVIDERS = [
     StatusModule,
     NavigationModule
   ],
-  exports: [
-  ],
-  providers: [ // expose our Services and Providers into Angular's dependency injection
+  exports: [],
+  providers: [
+    // expose our Services and Providers into Angular's dependency injection
     I18N_PROVIDERS,
     I18nObservers,
     DashboardObservers,
@@ -130,13 +136,12 @@ export const I18N_PROVIDERS = [
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptor,
       multi: true
-    },
+    }
   ],
-  entryComponents: [
-
-  ]
+  entryComponents: []
 })
 export class AppModule {
-  constructor(public appRef: ApplicationRef) { console.log(appRef); }
-
+  constructor(public appRef: ApplicationRef) {
+    console.log(appRef);
+  }
 }
--- src/app/views/local-settings/local-settings.component.ts
+++ src/app/views/local-settings/local-settings.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { I18nActions } from '../../shared/i18n/actions';
+import { Component, OnInit, NgZone } from "@angular/core";
+import { I18nActions } from "../../shared/i18n/actions";
 
-import { dispatch, select } from '@angular-redux/store';
-import { LocalSettingsActions } from './api/actions';
+import { dispatch, select } from "@angular-redux/store";
+import { LocalSettingsActions } from "./api/actions";
 
 @Component({
-  selector: 'app-local-settings',
-  templateUrl: './local-settings.component.html',
-  styleUrls: ['./local-settings.component.scss']
+  selector: "app-local-settings",
+  templateUrl: "./local-settings.component.html",
+  styleUrls: ["./local-settings.component.scss"]
 })
 export class LocalSettingsComponent implements OnInit {
   @select() settings;
 
-  constructor(private i18nActions: I18nActions,
-              private settingsActions: LocalSettingsActions) {}
+  constructor(
+    private i18nActions: I18nActions,
+    private settingsActions: LocalSettingsActions,
+    public zone: NgZone
+  ) {}
 
   ngOnInit() {
-    console.log('LocalSettingsComponent initialized.');
+    console.log("LocalSettingsComponent initialized.");
   }
 
   changeLanguage(lang) {
-    this.i18nActions.languageChange(lang);
+    localStorage.setItem("lang", lang);
+    document.location.reload();
   }
 
   activateCompactView() {
