d0e0174bb4bb66aad1245a44afcf49aa6092ceda
ahadortiz <ahadortiz@outlook.com>
2018-02-28 07:34:30 -0300

    remove links

--- src/app/views/about/about.component.ts
+++ src/app/views/about/about.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { environment } from '../../../environments/environment';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../environments/environment";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"]
 })
 export class AboutComponent implements OnInit {
   buildVersion: string;
+  content: any = "";
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.http
+      .get("assets/CHANGELOG.md", { responseType: "text" })
+      .subscribe(res => {
+        // remove all links
+        const patt = /\(http[^)]*\)/gi;
+        this.content = res.toString().replace(patt, "");
+      });
+  }
 
   ngOnInit() {
-    console.log('AboutComponent initialized.');
+    console.log("AboutComponent initialized.");
     this.buildVersion = environment.version;
   }
 }
