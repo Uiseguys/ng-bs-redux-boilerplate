daaf4a8aab7b30e49e3b29294a8a607ecc59461f
Divyesh Bhalala <bhalaladivs@gmail.com>
2018-03-06 10:06:20 +0530

    added bs config files

--- bs-config.js
+++ bs-config.js
@@ -1,7 +1,12 @@
 module.exports = {
 	port: process.env.PORT,
+	open: false,
+	logLevel: "silent",
 	files: ['./**/*.{html,htm,css,js}'],
 	server:{
-		baseDir: "./dist"
+		baseDir: "./dist",
+		middleware: {
+			0: null
+		}
 	}
 };
