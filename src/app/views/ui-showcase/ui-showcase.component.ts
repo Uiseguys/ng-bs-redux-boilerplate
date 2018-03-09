78eb6cd21daee9ff6750789f130b32d8081c18ed
Divyesh Bhalala <bhalaladivs@gmail.com>
2018-03-09 22:06:24 +0530

    added component to UI showcase

--- src/app/views/ui-showcase/ui-showcase.component.ts
+++ src/app/views/ui-showcase/ui-showcase.component.ts
@@ -8,6 +8,117 @@ import { Component, OnInit } from '@angular/core';
 export class UIShowcaseComponent implements OnInit {
 
     constructor() {}
+    breadcrumbItems: any = [
+        { active: false, href: '/alerts', target: 'blank', title: 'Alerts' },
+        { active: false, href: '/badge', target: 'blank', title: 'Badge' },
+        { active: true, href: '/breadcrumb', target: 'blank', title: 'Breadcrumbs' },
+    ];
+    cards: any[] = [
+        {
+            cardHeaderId: 'heading#1',
+            cardBodyId: 'collapse#1',
+            cardHeaderContent: 'Card Item #1',
+            cardBodyContent: 'Card Body #1',
+            showOnInit: true
+        },
+        {
+            cardHeaderId: 'heading#2',
+            cardBodyId: 'collapse#2',
+            cardHeaderContent: 'Card Item #2',
+            cardBodyContent: 'Card Body #2',
+            showOnInit: false
+        },
+        {
+            cardHeaderId: 'heading#3',
+            cardBodyId: 'collapse#3',
+            cardHeaderContent: 'Card Item #3',
+            cardBodyContent: 'Card Body #3',
+            showOnInit: false
+        }
+    ];
+    multiSelectData: any = ['Alex', 'Alabama', 'Alaska', 'andreas', 'alexandro'];
+    markdownContent: string = '# Header1' +
+        ' \n This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.5.' +
+        '\n ## Header 2' +
+        '\n Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).' +
+        '\n ## Running end-to-end tests' +
+        '\n Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).' +
+        '\n ### Bug Fixes' +
+        '\n * **common:** more detailed info about error' +
+        '\n `fix(common): more detailed info about error`';
+    thumbnail: any = {
+        0: {
+            src: 'https://www.w3schools.com/howto/img_fjords.jpg',
+            style: {
+                left: '-60px',
+                width: '600px',
+                height: '68px',
+                clip: 'rect(0, 120px, 68px, 0)'
+            }
+        },
+        1: {
+            style: {
+                left: '-180px',
+                clip: 'rect(0, 240px, 68px, 120px)'
+            }
+        },
+        2: {
+            style: {
+                left: '-300px',
+                clip: 'rect(0, 360px, 68px, 240px)'
+            }
+        },
+        3: {
+            style: {
+                left: '-420px',
+                clip: 'rect(0, 480px, 68px, 360px)'
+            }
+        },
+        4: {
+            style: {
+                left: '-540px',
+                clip: 'rect(0, 600px, 68px, 480px)'
+            }
+        }
+    };
+    progress: any = 10;
+    theme: String = 'berlin';
+    progressBarText: String = '50';
+    progressBarType: String = 'success';
+    progressBarStriped: boolean = true;
+    progressBarAnimated: boolean = true;
+    progressBarStriped1: boolean = false;
+    progressBarAnimated1: boolean = false;
+    getUser2Template() {
+        return `<div class="card card-18">
+            <img class="card-img-top" src="<%=user.picture.large%>" alt="Card image cap" />
+                <div class="card-body">
+                    <h5 class="card-title capitalized"><%=user.name.first%> <%=user.name.last%></h5>
+                    <a href="#" class="btn btn-primary">Send message</a>
+                </div>
+            </div > `;
+
+    }
+    getUserTemplate() {
+        return `<div class="card col-md-6 col-sm-12">
+            <div class="card-body" >
+                <div class="media">
+                    <img class="d-flex mr-3 rounded" src="<%=user.picture.medium%>" alt="Generic placeholder image" />
+                    <div class="media-body">
+                        <h5 class="mt-0 capitalized"><%=user.name.first%> <%=user.name.last%></h5>
+
+                        <div>
+                            <span class="capitalized">
+                                <%=user.location.city%>, <%=user.location.state%>,
+                                </span>
+                            <span> <%=user.location.street%> </span>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div> `;
+
+    }
 
     ngOnInit() {
         console.log('UI Showcase Initialized');
